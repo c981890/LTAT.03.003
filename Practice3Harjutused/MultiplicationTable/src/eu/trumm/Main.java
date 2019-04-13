49439da5bac3bbe84511e582da87832c93520c1a
Aiki Trumm <aiki.trumm@mail.ee>
2019-04-13 10:55:51 +0300

    multiplication table clean code

--- Practice3Harjutused/MultiplicationTable/src/eu/trumm/Main.java
+++ Practice3Harjutused/MultiplicationTable/src/eu/trumm/Main.java
@@ -3,38 +3,31 @@ package eu.trumm;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println(multiplicationTable(10, 10));
+        System.out.println(multiplicationTable(4, 4));
     }
 
     static String multiplicationTable(int width, int height) {
         int cellWidth = String.valueOf(width * height).length() + 1;
-
-        String firstLine = String.format("%" + cellWidth + "s |", "*");
+        StringBuilder firstLine = new StringBuilder(String.format("%" + cellWidth + "s |", "*"));
 
         for (int l = 1; l <= width; l++) {
-            firstLine += String.format("%" + cellWidth + "s", l);
+            firstLine.append(String.format("%" + cellWidth + "s", l));
         }
 
-        int k = 1;
         String lineStart = "";
-        String formattedString = "";
-        String wholeLine = "";
+        String nextInLine = "";
+        StringBuilder wholeLine = new StringBuilder();
 
         for (int j = 1; j <= height; j++) {
 
-            String line = "";
+            StringBuilder numbersInLine = new StringBuilder();
             for (int i = 1; i <= width; i++) {
-                line += String.format("%" + cellWidth + "s", i * k);
+                numbersInLine.append(String.format("%" + cellWidth + "s", i * j));
             }
-            // lineStartTemp += "" + k + " |" + line + "\n";
-            lineStart = String.format("%" + cellWidth + "s |", k);
-            formattedString = line + "\n";
-            wholeLine += lineStart + formattedString;
-            k++;
+            lineStart = String.format("%" + cellWidth + "s |", j);
+            nextInLine = numbersInLine + "\n";
+            wholeLine.append(lineStart).append(nextInLine);
         }
-
-
         return firstLine + "\n" + wholeLine;
     }
-
 }
