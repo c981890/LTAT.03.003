d7990a92b192f70b52404bd150fbb93f087b7042
Aiki Trumm <aiki.trumm@mail.ee>
2019-04-15 22:10:52 +0300

    pig latin stream

--- Practice3Harjutused/PigLatin/src/eu/trumm/Main.java
+++ Practice3Harjutused/PigLatin/src/eu/trumm/Main.java
@@ -7,17 +7,16 @@ import java.util.stream.Stream;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println(pigLatinStream("!"));
+        System.out.println(pigLatinStream("moos riiul pä!ke"));
     }
 
 
     public static String pigLatinStream(String sentence) {
 
-        String changedSentence = Arrays.stream(sentence.split(" ")) // https://www.geeksforgeeks.org/arrays-stream-method-in-java/
+        return Arrays.stream(sentence.split(" ")) // https://www.geeksforgeeks.org/arrays-stream-method-in-java/
                 .map(word -> word.substring(1).concat(word.substring(0, 1)))// https://dzone.com/articles/how-to-use-map-filter-collect-of-stream-in-java-8
                 .map(word -> word.concat("ay"))
-                .collect(Collectors.joining(" ")); //https://www.baeldung.com/java-8-collectors
-        return changedSentence;
+                .collect(Collectors.joining(" "));
     }
 
 }
